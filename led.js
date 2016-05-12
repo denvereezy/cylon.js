01354cafe0bf4167926b2ed64717fcb9e3fd433e
denver daniels <denver@projectcodex.co>
2016-05-12 15:01:17 +0200

    led blinking

---
+++ led.js
@@ -0,0 +1,17 @@
+var Cylon = require("cylon");
+
+Cylon.robot({
+  connections: {
+    arduino: { adaptor: 'firmata', port: '/dev/ttyACM0' }
+  },
+
+  devices: {
+    led: { driver: 'led', pin: 13 }
+  },
+
+  work: function(my) {
+    every((1).second(), function() {
+      my.led.toggle();
+    });
+  }
+}).start();
