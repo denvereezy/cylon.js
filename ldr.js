30b5e4a8252875c65876f2942a44018d3d53d469
Denver Daniels <denver@projectcodex.co>
2016-09-02 13:41:51 +0200

    Create ldr.js

---
+++ ldr.js
@@ -0,0 +1,32 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connections: {
+    arduino: { adaptor: 'firmata', port: '/dev/ttyACM0' }
+  },
+
+  devices: {
+    sensor: { driver: 'analog-sensor', pin: 0, lowerLimit: 100, upperLimit: 900 },
+    led: { driver: 'led', pin: 11 }
+  },
+
+  work: function(my) {
+    var analogValue = 0;
+
+    every((1).second(), function() {
+      analogValue = my.sensor.analogRead();
+      console.log('Analog value => ', analogValue);
+      my.led.brightness(analogValue);
+    });
+
+    my.sensor.on('lowerLimit', function(val) {
+      console.log("Lower limit reached!");
+      console.log('Analog value => ', val);
+    });
+
+    my.sensor.on('upperLimit', function(val) {
+      console.log("Upper limit reached!");
+      console.log('Analog value => ', val);
+    });
+  }
+}).start();
